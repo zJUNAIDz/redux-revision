7184c9fa088de46356260de70ce9473edbe1c53f
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-09-05 16:14:02 +0530

    immer: use immer for reducer for state mutation it helps mutating the immutable state like it is mutable (lets us avoid lots of spread operators in objec, especially nested ones)

--- tip-calculator/src/store/items/reducer.js
+++ tip-calculator/src/store/items/reducer.js
@@ -4,7 +4,7 @@ import {
   ITEM_QUANTITY_UPDATED,
   ITEM_REMOVED
 } from './actions';
-
+import { produce } from 'immer';
 let id = 1;
 
 export const initialItems = [
@@ -12,30 +12,46 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham Red', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
-  switch (action.type) {
-    case ITEM_ADDED:
-      const item = { uuid: id++, quantity: 1, ...action.payload };
-      return [...state, item];
-    case ITEM_REMOVED:
-      const newItem = state.filter((item) => action.payload.uuid !== item.uuid);
-      console.log({ newItem });
-      return newItem;
-    case ITEM_PRICE_UPDATED:
-      return state.map((item) =>
-        item.uuid === action.payload.uuid
-          ? { ...item, price: action.payload.price }
-          : item
-      );
-    case ITEM_QUANTITY_UPDATED:
-      return state.map((item) =>
-        item.uuid === action.payload.uuid
-          ? { ...item, quantity: action.payload.quantity }
-          : item
-      );
-    default:
-      return state;
-  }
-};
+// export const reducer = (state = initialItems, action) => {
+//   switch (action.type) {
+//     case ITEM_ADDED:
+//       return produce(state, (draft) => {
+//         const item = { uuid: id++, quantity: 1, ...action.payload };
+//         draft.push(item);
+//       });
+//     case ITEM_REMOVED:
+//       return state.filter((item) => action.payload.uuid !== item.uuid);
+//     case ITEM_PRICE_UPDATED:
+//       return produce(state, (draft) => {
+//         const item = draft.find((item) => item.uuid === action.payload.uuid);
+//         item.price = parseInt(action.payload.price);
+//       });
 
+//     case ITEM_QUANTITY_UPDATED:
+//       return produce(state, (draft) => {
+//         const item = draft.find((item) => item.uuid === action.payload.uuid);
+//         item.quantity = parseInt(action.payload.quantity);
+//       });
+//     default:
+//       return state;
+//   }
+// };
+export const reducer = produce((state, action) => {
+  const { type } = action;
+
+  if (type === ITEM_ADDED) {
+    state.push({ uuid: id++, quantity: 1, ...action.payload });
+  }
+  if (type === ITEM_REMOVED) {
+    state.filter((item) => action.payload.uuid !== item.uuid);
+  }
+  if (type === ITEM_PRICE_UPDATED) {
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price);
+  }
+  if (type === ITEM_QUANTITY_UPDATED) {
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity);
+  }
+}, initialItems);
 export default reducer;
